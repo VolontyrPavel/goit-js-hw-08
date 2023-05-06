456536d80c89fb3c447748cc14f777fd564a6ec5
VolontyrPavel <pavel.volontyr@gmail.com>
2023-05-06 14:03:50 +0300

    begin

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// консти
+const container = document.querySelector('.gallery');
+const cardImg = createGalleryCard (galleryItems);
+
+container.insertAdjacentHTML('beforeend', cardImg);
+
+// функція створення розмітки
+function createGalleryCard (galleryItems) {
+    
+    return galleryItems.map(({ preview, original, description}) => {
+        return `
+        <li class="gallery__item list">
+            <a class="gallery__link link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    alt="${description}"
+                />
+            </a>
+        </li>
+        `
+    }).join('');
+};
+    
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 });
