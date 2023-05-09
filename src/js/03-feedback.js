1bdd9693bd4e85630c223467926afb8331226a0d
VolontyrPavel <pavel.volontyr@gmail.com>
2023-05-09 23:07:37 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,40 +1,35 @@
 import throttle from 'lodash.throttle';
 
-const refs = {
-    form: document.querySelector('.feedback-form'),
-    email: document.querySelector('input'),
-    textarea: document.querySelector('textarea'),
-}
-
-const user = {
-    email: refs.email.value,
-    message: refs.textarea.value,
-};
-
 const STORAGE_KEY = 'feedback-form-state';
 
-cleanTextInMemory();
+const refs = document.querySelector('.feedback-form');
 
-refs.form.addEventListener('input', throttle(onInput, 500));
-refs.form.addEventListener('submit', onUserDataSubmit);
+refs.addEventListener('input', throttle(onInput, 500));
+refs.addEventListener('submit', onUserDataSubmit);
 
-function onUserDataSubmit(e) {
-    e.preventDefault();
-    e.currentTarget.reset();
-    console.log(user);
-    localStorage.removeItem(STORAGE_KEY);
+textInMemory(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+
+function textInMemory(data) {
+    if (data) {
+        refs.email.value = data.email;
+        refs.message.value = data.message;
+    }
 }
 
 function onInput(e) {
-    user[e.target.name] = e.target.value;
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(user))
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(setUser()));
 }
 
-function cleanTextInMemory() {
-    const textInMemory = JSON.parse(localStorage.getItem(STORAGE_KEY));
-    console.log(textInMemory);
-    if (textInMemory) {
-        refs.email.value = textInMemory.email;
-        refs.textarea.value = textInMemory.message;
-    }
+function onUserDataSubmit(e) {
+    e.preventDefault();
+    console.log(setUser());
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function setUser() {
+    return {
+        email: refs.email.value,
+        message: refs.message.value,
+    };
 }
